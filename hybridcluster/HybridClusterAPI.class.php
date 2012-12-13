88507cb32bcd95f565bcdb25c8618ff951b7474c
Rob Haswell <me@robhaswell.co.uk>
2012-12-13 16:59:15 +0000

    Debuggering

--- hybridcluster/HybridClusterAPI.class.php
+++ hybridcluster/HybridClusterAPI.class.php
@@ -31,7 +31,7 @@ class HybridClusterAPI {
         $response_str = HTTP::post($url = "{$this->endpoint}/{$method}", $parameters);
 
         if (!strlen($response_str))
-            throw new HybridClusterAPIException("Empty response received");
+            throw new HybridClusterAPIException("Empty response received from {$url}");
 
         $data = json_decode($response_str);
         logModuleCall("hybridcluster", $url, $parameters, $response_str, $data, Array($this->apikey));
